878843dec0eac0e46a778bd7e66f086563bce7b4
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2023-07-08 11:36:08 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBz25reMNfYzOmwYS1x00uO40QqdNIJl2E",
+  authDomain: "e-commerce-6a4a0.firebaseapp.com",
+  projectId: "e-commerce-6a4a0",
+  storageBucket: "e-commerce-6a4a0.appspot.com",
+  messagingSenderId: "820745642437",
+  appId: "1:820745642437:web:4b62d6fb003085fec7ebac"
+};
+
+
+// Persons
+// accounts
+
+
+// rulls if true
